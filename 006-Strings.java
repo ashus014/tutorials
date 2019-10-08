49f10bddced574831dcf3a4db7c6a573ab9d7e04
ashus014 <ashu.ask.singh@gmail.com>
2019-10-08 18:28:49 +0530

    Create 006-Strings.java

---
+++ 006-Strings.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+
+        //common - int,long,boolean
+
+        //another data type is String ; It is not the primitive data type.
+
+        String myString = "This is a String";
+        System.out.println("My String is ::  " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("My String is ::  " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        
+        //String in JAVA is immutable
+
+    }
+}
