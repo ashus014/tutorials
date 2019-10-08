9f10271b349f1e69d79117b1b0917d44ddb51694
ashus014 <ashu.ask.singh@gmail.com>
2019-10-08 18:00:05 +0530

    Create 005-CharAndBoolean.java

---
+++ 005-CharAndBoolean.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        //MUCH MORE RELEVANT DURING 1990S to save memory
+        //occupies 2 bytes of memory
+        //unicode-table.com
+
+        char myUniCodeChar = '\u0044';
+
+        System.out.println(myChar);
+        System.out.println(myUniCodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+        //basically used for this purpose.
+
+
+
+
+
+    }
+}
