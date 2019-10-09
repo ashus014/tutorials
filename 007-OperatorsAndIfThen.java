9d5ce7adb47edfe99647d04e0f0b9f7154af0f6f
ashus014 <ashu.ask.singh@gmail.com>
2019-10-09 21:27:23 +0530

    Create 007-OperatorsAndIfThen.java

---
+++ 007-OperatorsAndIfThen.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 2;
+
+        result--;
+        System.out.println("Result :: " + result);
+
+        result++;
+        System.out.println("Result :: " + result);
+
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am scared of Aliens");
+        }
+
+    }
+}
