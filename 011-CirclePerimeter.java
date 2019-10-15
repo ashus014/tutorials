34944e08269c384adca7748be75d614e123ef380
ashus014 <ashu.ask.singh@gmail.com>
2019-10-15 12:43:48 +0530

    Create 011-CirclePerimeter.java

---
+++ 011-CirclePerimeter.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Perimeter(7);
+    }
+
+    public static  void Perimeter(int radius){
+        Double Perimeter = 3.14 * 2 * radius;
+        System.out.println("Perimeter : " + Perimeter);
+    }
+}
