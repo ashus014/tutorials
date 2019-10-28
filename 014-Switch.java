241673b9cb845486a43a9d101751a4015f1af5ad
ashus014 <ashu.ask.singh@gmail.com>
2019-10-29 05:10:25 +0530

    Create 014-Switch.java

---
+++ 014-Switch.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            String month = "January";
+            switch (month){
+                case "January":
+                    System.out.println("Jan");
+                    break;
+                case "June":
+                    System.out.println("June");
+                    break;
+                default:
+                    System.out.println("Not Sure");
+            }
+    }
+}
